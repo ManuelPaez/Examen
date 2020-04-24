cb51d749550dfbed7063d61d419aa7e79e515331
ManuelPaez <61057699+ManuelPaez@users.noreply.github.com>
2020-04-24 12:51:55 -0500

    Create heroe.js

---
+++ heroe.js
@@ -0,0 +1,61 @@
+class Heroe{
+constructor(){
+
+this.x=15;
+this.y= height/2;
+this.velx=4;
+this.img=[];
+this.vely=6;
+this.frame=0;
+this.dir=0.5;
+
+for(var i=0; i<19; i++){
+this.img[i]= loadImage("heroe/heroe_"+i+".gif");
+}
+}
+
+mostrar(){
+push();
+translate(this.x, this.y);
+scale(this.dir,0.5);
+imageMode(CENTER);
+image(this.img[this.frame], 0,0);
+pop();
+}
+
+derecha(){ 
+this.frame++;
+this.x=this.x + this.velx;
+this.dir = 0.5;
+if(this.frame> 7){
+this.frame=0;
+}
+}
+izquierda(){
+this.frame++;
+this.x=this.x-this.velx;
+this.dir=-0.5;
+if(this.frame>7){
+this.frame=0;
+}
+}
+ arriba(){
+   this.frame++;
+    this.y=this.y-this.vely;
+    this.dir=1;
+   this.dir = 0.5;
+if(this.frame> 7){
+this.frame=0;
+}
+    
+  }
+  abajo(){
+    this.frame++;
+    this.y=this.y + this.vely;
+    this.dir=1;
+    this.dir = 0.5;
+if(this.frame> 7){
+this.frame=0;
+}
+  }
+}
